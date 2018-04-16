fce5bc10513c276ff3d77b2ea33b5914c1654612
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-04-16 16:07:51 +0200

    fix: circleci config

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o xtrace
 
 # Do not run this script outside CircleCI
 test -n "$CIRCLE_USERNAME" || exit 1
@@ -7,8 +11,9 @@ ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 git config --global user.email "cittadinanzadigitale@italia.it"
 git config --global user.name "$CIRCLE_USERNAME"
 git checkout -B website
+git pull origin website
 git add -f _site Dockerfile
 git commit -m "[website update] v$CIRCLE_BUILD_NUM"
-git push -f -u origin website:website
+git push -u origin website:website
 git tag -a website-v$CIRCLE_BUILD_NUM -m "[website update] v$CIRCLE_BUILD_NUM"
 git push origin website-v$CIRCLE_BUILD_NUM
