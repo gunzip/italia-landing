b30a6f918b939ac11ad1a7f58921a77b11b6c9e6
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-04-16 16:21:16 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,11 @@ set -o xtrace
 test -n "$CIRCLE_USERNAME" || exit 1
 
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-git config --global user.email "cittadinanzadigitale@italia.it"
+git config --global user.email $GIT_USER_EMAIL
 git config --global user.name "$CIRCLE_USERNAME"
-git checkout -B website
-git pull origin website
+git checkout -B $WEBSITE_BRANCH
 git add -f _site Dockerfile
 git commit -m "[website update] v$CIRCLE_BUILD_NUM"
-git push -u origin website:website
+git push -u origin $WEBSITE_BRANCH:$WEBSITE_BRANCH
 git tag -a website-v$CIRCLE_BUILD_NUM -m "[website update] v$CIRCLE_BUILD_NUM"
 git push origin website-v$CIRCLE_BUILD_NUM
