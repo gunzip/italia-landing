75be91226673d9372d8cbcfde433d801af460cf9
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-04-16 16:30:38 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,8 @@ ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name "$CIRCLE_USERNAME"
 git checkout -B $WEBSITE_BRANCH
-git pull origin $WEBSITE_BRANCH
 git add -f _site Dockerfile
 git commit -m "[website update] v$CIRCLE_BUILD_NUM"
-git push -u origin $WEBSITE_BRANCH:$WEBSITE_BRANCH
+git push -f -u origin $WEBSITE_BRANCH:$WEBSITE_BRANCH
 git tag -a website-v$CIRCLE_BUILD_NUM -m "[website update] v$CIRCLE_BUILD_NUM"
 git push origin website-v$CIRCLE_BUILD_NUM
